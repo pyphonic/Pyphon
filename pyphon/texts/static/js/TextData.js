339dc6f36310031a4e7e6d6bb2fe4a47b1697889
Julien <julienawilson@gmail.com>
2017-02-11 17:18:22 -0800

    text object for rendering

--- pyphon/texts/static/js/TextData.js
+++ pyphon/texts/static/js/TextData.js
@@ -4,17 +4,31 @@
 'use strict';
 
 
-var textData = {}
+function textData(text){
+    this.time = text.time;
+    this.body = text.body;
+    this.sender = text.sender;
+ };
 
 textData.allTexts = []
 
 textData.callAPI = function(){
     //Call the Api to grab recent texts
     $.get('/api/texts', function(data,msg,xhr){
-      localStorage.setItem('allTexts', JSON.stringify(data));
-      console.log(data);
-      return data;
-      });
-    };
+
+        console.log(data);
+        textData.allTexts = data.map(function(data,idx,array){
+          return new textData(data);
+        textData.renderData(textData.allTexts)
+        });
+    });
+};
+
+textData.renderData = function(textsArray){
+    textsArray.sort(function(a, b){
+      return b.time - a.time;
+    });
+    $('#past_texts .message').remove();
     
-textData.renderData
+
+}
