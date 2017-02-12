5e49c7780ea058780893f4faeb9325f56619c71a
Julien <julienawilson@gmail.com>
2017-02-11 19:32:02 -0800

    ajax brings in data!

--- pyphon/texts/static/js/TextData.js
+++ pyphon/texts/static/js/TextData.js
@@ -15,20 +15,32 @@ textData.allTexts = []
 textData.callAPI = function(){
     //Call the Api to grab recent texts
     $.get('/api/texts', function(data,msg,xhr){
-
-        console.log(data);
         textData.allTexts = data.map(function(data,idx,array){
           return new textData(data);
-        textData.renderData(textData.allTexts)
         });
+        textData.renderData(textData.allTexts);
     });
 };
 
 textData.renderData = function(textsArray){
+    //Take all of the texts and render them to the page.
     textsArray.sort(function(a, b){
       return b.time - a.time;
     });
     $('#past_texts .message').remove();
-    
-
+    // textsArray.map(function(data){
+    //     $('#past_texts').append(textData.renderMessage(data));
+    // });
+    for (var i=0; i<textsArray.length; i++){
+        var message = textsArray[i];
+        var mes_in_temp = textData.renderMessage(message);
+        var appendTo = $('#past_texts');
+        appendTo.append(mes_in_temp);
+    };
 }
+
+textData.renderMessage = function(data){
+    var source = $('#text-template').html();
+    var template = Handlebars.compile(source);
+    return template(data);
+};
