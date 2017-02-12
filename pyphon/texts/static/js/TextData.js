738bbf2f3b41dcb18a641fa269b1330eebca2179
Julien <julienawilson@gmail.com>
2017-02-12 15:06:50 -0800

    text page supports post, refresh safe

--- pyphon/texts/static/js/TextData.js
+++ pyphon/texts/static/js/TextData.js
@@ -53,23 +53,16 @@ $(document).ready(function(){
     var form = $("form");
     form.submit(function(e){
         e.preventDefault();
+
         $.ajax({
-            url: '/texts',
+            url: '/texts/',
             type: "POST",
-            data:{
-                // "csrf_token": $(this).parent().find("input[name='csrf_token']")[0].value,
-                "body": body
-            },
+            data: form.serialize(),
+
             success: function(){
-            //     var href = $('h2 a').attr('href').split('/').slice(1);
-            //     var new_post_id = Number(href[1]) + 1;
-            //     var new_post_href = href[0] + '/' + new_post_id.toString();
-            //     post_template = '<h2><a href="' + new_post_href + '">' + title + '</a></h2>'+
-            //         '<p class="lead">by <a href="/about_me">Julien Wilson</a></p>'+
-            //         '<p><span class="glyphicon glyphicon-time"></span> Posted Just Now</p>'+
-            //         '<br/>'
-            //     $('#posts').prepend(post_template);
-                $('form')[0].reset();
+                console.log('The text was sent!')
+                console.log(form)
+                form[0].reset();
             },
             error: function(err){
                 console.error(err);
