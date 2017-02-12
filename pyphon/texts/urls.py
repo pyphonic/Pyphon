53ab56df9b7fa5211ca874e562d8ad2d67d55441
Maelle Vance <maellevance@gmail.com>
2017-02-11 17:36:35 -0800

    starting a hook view with ngrok

--- pyphon/texts/urls.py
+++ pyphon/texts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from texts.views import text_view
+from texts.views import text_view, ProcessHookView
 
 urlpatterns = [
     url(r'^$', text_view, name="texts"),
+    url(r'^hook/', ProcessHookView.as_view(), name="text_hook")
 ]
--- pyphon/texts/views.py
+++ pyphon/texts/views.py
@@ -1,5 +1,10 @@
+import json
+
 from django.shortcuts import render
 from texts.models import Text
+from django.http import HttpResponse
+from django.views.generic import View
+from braces.views import CsrfExemptMixin
 
 
 def text_view(request):
@@ -7,3 +12,9 @@ def text_view(request):
     texts = Text.objects.all()
     return render(request, "texts/texting.html", {
         'texts': texts})
+
+
+class ProcessHookView(CsrfExemptMixin, View):
+    def post(self, request, args, *kwargs):
+        print(json.loads(request.body))
+        return HttpResponse()
