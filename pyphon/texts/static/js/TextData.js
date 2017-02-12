e3b4c0d1a83a5ffd6a488535106b9c3b8b3dbb8c
Julien <julienawilson@gmail.com>
2017-02-11 16:28:11 -0800

    ajax calls api, working on etag and timer

--- pyphon/texts/static/js/TextData.js
+++ pyphon/texts/static/js/TextData.js
@@ -4,20 +4,17 @@
 'use strict';
 
 
-textData = {}
+var textData = {}
 
-
+textData.allTexts = []
 
 textData.callAPI = function(){
     //Call the Api to grab recent texts
     $.get('/api/texts', function(data,msg,xhr){
-      var lastMod = xhr.getResponseHeader('Last-Modified');
-      localStorage.setItem('lastMod', lastMod);
-      localStorage.setItem('allIncidents', JSON.stringify(data));
-      var newdataarray = data.map(function(elem,idx,array){
-        return new policeData(elem);
+      localStorage.setItem('allTexts', JSON.stringify(data));
+      console.log(data);
+      return data;
       });
-      policeData.loadData(newdataarray);
-      policeData.buildDatabase();
-    });
     };
+    
+textData.renderData
