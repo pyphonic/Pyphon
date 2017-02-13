20a461e49b99f9e7a7455a28dfe716eb8166ee5c
Julien <julienawilson@gmail.com>
2017-02-13 13:39:29 -0800

    contact detail template, begin view

--- pyphon/contacts/views.py
+++ pyphon/contacts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView, UpdateView
 
-# Create your views here.
+from contacts.models import Contact
+
+
+# # Create your views here.
+# class ContactIdView(TemplateView):
+#     """Class based view for individual photo view."""
+
+#     template_name = "imager_images/photo_id.html"
+
+#     def get_context_data(self, pk):
+#         """Extending get_context_data method for our data."""
+#         contact = Contact.objects.get(pk=pk)
+#         if photo.published == 'public' or photo.owner.user == self.request.user:
+#             return {"photo": photo}
+#         else:
+#             error = "I'm sorry, that photo is not available."
+#             return {"error": error}
