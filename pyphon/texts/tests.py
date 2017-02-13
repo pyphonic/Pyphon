5f56fb7fd23ed6c53fb61b8548f83cdc604c3847
Julien <julienawilson@gmail.com>
2017-02-13 12:11:39 -0800

    building text page for each contact

--- pyphon/texts/tests.py
+++ pyphon/texts/tests.py
@@ -4,7 +4,10 @@ from django.urls import reverse_lazy
 from texts.models import Text
 from texts.views import TextView
 
+from contacts.models import Contact
+
 import datetime
+
 
 class TextTestCase(TestCase):
     """The Text app test runner."""
@@ -47,40 +50,58 @@ class TextTestCase(TestCase):
         text1.save()
         self.assertTrue(text1.__repr__() == text1.body[:20])
     
-    def test_text_view_returns_text(self):
-        """Test that text view returns a text."""
-        text1 = Text(body="Jabba no watta.", sender="them")
-        text1.save()
-        text2 = Text(body="Too Nakma Noya Solo!", sender="you")
-        text2.save()
-        view = TextView.as_view()
-        req = self.request.get(reverse_lazy('texts'))
-        response = view(req)
-        self.assertTrue(response.context_data['texts'].count() == 2)
+    # def test_text_view_returns_text(self):
+    #     """Test that text view returns a text."""
+    #     text1 = Text(body="Jabba no watta.", sender="them")
+    #     text1.save()
+    #     text2 = Text(body="Too Nakma Noya Solo!", sender="you")
+    #     text2.save()
+    #     view = TextView.as_view()
+    #     req = self.request.get(reverse_lazy('texts'))
+    #     response = view(req)
+    #     self.assertTrue(response.context_data['texts'].count() == 2)
 
-    def test_text_view_returns_two_texts(self):
-        """Test that text view returns two texts."""
-        text1 = Text(body="Jabba no watta.", sender="them")
-        text1.save()
-        view = TextView.as_view()
-        req = self.request.get(reverse_lazy('texts'))
-        response = view(req)
-        self.assertTrue(response.context_data['texts'].count() == 1)
+    # def test_text_view_returns_two_texts(self):
+    #     """Test that text view returns two texts."""
+    #     text1 = Text(body="Jabba no watta.", sender="them")
+    #     text1.save()
+    #     view = TextView.as_view()
+    #     req = self.request.get(reverse_lazy('texts'))
+    #     response = view(req)
+    #     self.assertTrue(response.context_data['texts'].count() == 1)
 
-    def test_text_view_status_200(self):
-        """Test that text view returns ok status."""
-        text1 = Text(body="Jabba no watta.", sender="them")
-        text1.save()
-        view = TextView.as_view()
-        req = self.request.get(reverse_lazy('texts'))
-        response = view(req)
-        self.assertTrue(response.status_code == 200)
+    # def test_text_view_status_200(self):
+    #     """Test that text view returns ok status."""
+    #     text1 = Text(body="Jabba no watta.", sender="them")
+    #     text1.save()
+    #     view = TextView.as_view()
+    #     req = self.request.get(reverse_lazy('texts'))
+    #     response = view(req)
+    #     self.assertTrue(response.status_code == 200)
 
-    def test_text_view_template(self):
-        """Test that text view uses texts template."""
-        text1 = Text(body="Jabba no watta.", sender="them")
-        text1.save()
-        response = self.client.get(reverse_lazy('texts'))
-        self.assertTemplateUsed(response, 'texts/texting.html')
+    # def test_text_view_template(self):
+    #     """Test that text view uses texts template."""
+    #     text1 = Text(body="Jabba no watta.", sender="them")
+    #     text1.save()
+    #     response = self.client.get(reverse_lazy('texts'))
+    #     self.assertTemplateUsed(response, 'texts/texting.html')
 
 # Needs tests that use self.client and bs4 to count texts on page
+
+    def create_new_contact(self):
+        """Create a contact for testing."""
+        jabba = Contact(number='+15555555555')
+        jabba.save()
+        return jabba.id
+
+    def submit_new_text_form(self):
+        """Submit a new text form."""
+        contact_id = self.create_new_contact()
+        response = self.client.post(reverse_lazy('texts', kwargs={'pk': contact_id},), {'body': '6 Ploon, toh eenteen. Il yabba ma dookee Mahs tah, icht boong'})
+        return response
+
+    def test_add_a_text_count(self):
+        """Test that adding a text increases the model count."""
+        texts = Text.objects.count()
+        self.submit_new_text_form()
+        assert Text.objects.count() == texts + 1
