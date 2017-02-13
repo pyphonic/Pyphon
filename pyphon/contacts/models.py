687e3af38c0dfea8518884b56d7c4b6be7c93579
Maelle Vance <maellevance@gmail.com>
2017-02-12 16:55:14 -0800

    foreign key nullable

--- pyphon/contacts/models.py
+++ pyphon/contacts/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from phonenumber-field.modelfields import PhoneNumberField
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 
 
 class Contact(models.Model):
     """Text messages."""
-    name = CharField(max_length=255, default="")
+    name = models.CharField(max_length=255, default="")
     number = PhoneNumberField()
 
     def __repr__(self):
