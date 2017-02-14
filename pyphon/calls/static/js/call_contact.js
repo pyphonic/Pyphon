e58594d4c4ca0b0ce7055b23982f0a9bb44ce6e9
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-02-14 15:37:17 -0800

    calling out from detail page

--- pyphon/calls/static/js/call_contact.js
+++ pyphon/calls/static/js/call_contact.js
@@ -2,7 +2,7 @@
 
 //Grab the phone number and connect using it as a param.
 function makeCall() {
-    var phoneNumber = $('#callme').val()
+    var phoneNumber = $('#callme').text()
     console.log('calling' + phoneNumber)
     var params = {"phoneNumber": phoneNumber};
     Twilio.Device.connect(params);
@@ -13,14 +13,13 @@ function makeCall() {
 $('#call_button').click(function() {
     console.log('outgoing call')
     makeCall();
-    var name = $('#name').val()
-    var phone_number = $('#callme').val()
+    var name = $('#name').text()
+    var phone_number = $('#callme').text()
     $('#incoming').slideDown();
-    if (thisContact.name) {
-        $("#contact").text(thisContact[0].name);
-    } else {
+    if (name == "No Name"){
         $("#contact").text(phone_number);
+    } else {
+        $("#contact").text(name);
     }
-    });
     $("#hangupbutton").fadeIn();
 });
