84d804305c78aa6301015fbd26db8830d27c024a
Julien <julienawilson@gmail.com>
2017-02-14 15:42:10 -0800

    starting ansible deployment, not running server

---
+++ ansible/templates/gunicornscript.sh
@@ -0,0 +1,17 @@
+APPNAME=pyphon
+APPDIR=/home/ubuntu/$APPNAME/
+
+LOGFILE=$APPDIR'gunicorn.log'
+ERRORFILE=$APPDIR'gunicorn-error.log'
+
+NUM_WORKERS=3
+
+ADDRESS=127.0.0.1:8000
+
+cd $APPDIR
+
+
+exec gunicorn pyphon.wsgi:application \
+-w $NUM_WORKERS --bind=$ADDRESS \
+--log-level=debug \
+--log-file=$LOGFILE 2>>$LOGFILE  1>>$ERRORFILE &
