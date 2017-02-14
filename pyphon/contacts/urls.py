ada71fe25329444edd9a6fae77698a227e1b86a4
Julien <julienawilson@gmail.com>
2017-02-13 16:41:06 -0800

    starting contact edit

--- pyphon/contacts/urls.py
+++ pyphon/contacts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/$', ContactIdView.as_view(), name="contact_detail"),
     url(r'^$', ContactListView.as_view(), name='contacts'),
     url(r'^new/$', ContactAddView.as_view(), name='new_contact'),
+    url(r'^edit/$', ContactEditView.as_view(), name='edit_contact'),
 ]
