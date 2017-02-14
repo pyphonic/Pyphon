76137f7d37577a469c13d5ad8a407de1e6ba8973
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-02-14 10:34:24 -0800

    updating ux for incoming calls

--- pyphon/calls/static/js/calls.js
+++ pyphon/calls/static/js/calls.js
@@ -1,3 +1,6 @@
+// Make outgoing calls from the dial_screen.html page
+
+//Grab the phone number and connect using it as a param.
 function makeCall() {
     var phoneNumber = '+' + $('#numfield').val()
     console.log('calling' + phoneNumber)
@@ -6,15 +9,18 @@ function makeCall() {
 
 }
 
+//Grab the number from the number field
 function addNumber(element){
     $('#numfield').val($('#numfield').val()+element.value);
     console.log($('#numfield').val().length)
 }
 
+//Delete a number from the number field
 function deleteNumber(element){
     $('#numfield').val($('#numfield').val().slice(0, -1));
 }
 
+//Make an outgoing call
 $('#call_button').click(function() {
     console.log('outgoing call')
     makeCall();
@@ -23,7 +29,7 @@ $('#call_button').click(function() {
     $("#hangupbutton").fadeIn();
 });
 
-/* End a call */
+//End a call
 $("#hangupbutton").click(function () {
     Twilio.Device.disconnectAll();
 });
--- pyphon/calls/static/js/incoming.js
+++ pyphon/calls/static/js/incoming.js
@@ -1,3 +1,5 @@
+//Receive incoming calls on any page.
+
 /* Get a Twilio Client token with an AJAX request */
 $(document).ready(function() {
     $.get("/calls/token", {forPage: window.location.pathname}, function(data) {
@@ -7,32 +9,34 @@ $(document).ready(function() {
     });
 });
 
-// /* Report any errors to the call status display */
-// Twilio.Device.error(function (error) {
-//     updateCallStatus("ERROR: " + error.message);
-// });
-
 Twilio.Device.ready(function(device){
+    // Get device ready to receive incoming calls.
     console.log("Twilio.Device is now ready for connections");
 });
 
 Twilio.Device.incoming(function(connection) {
+    // Receive incoming call, slide down call screen.
     console.log('incoming call')
     $('#incoming').slideDown();
 
     $("#answerbutton").click(function() {
+        // Answer call
         connection.accept();
         $(".incoming_call").fadeOut();
         $("#hangupbutton").fadeIn();
     });
 
     $("#rejectbutton").click(function() {
+        // Reject call
         connection.reject();
         $('#incoming').slideUp();
     });
 });
+
 /* End a call */
 $("#hangupbutton").click(function () {
     Twilio.Device.disconnectAll();
     $('#incoming').slideUp();
+    $("#hangupbutton").fadeOut();
+    $(".incoming_call").fadeIn();
 });
