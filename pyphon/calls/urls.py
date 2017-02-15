7a664011d26e563a19277b6b571b2a526e12b2a8
Ford Fowler <fordjfowler@gmail.com>
2017-02-14 16:42:24 -0800

    change urls, css

--- pyphon/calls/urls.py
+++ pyphon/calls/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from calls.views import callview, get_token, call, CallListView
 
 urlpatterns = [
-    url(r'^$', callview, name="calls"),
+    url(r'^dial$', callview, name="calls"),
     url(r'^token$', get_token, name='token'),
     url(r'^call$', call, name='call'),
-    url(r'^call_list/$', CallListView.as_view(), name="call_list")
+    url(r'^$', CallListView.as_view(), name="call_list")
 ]
