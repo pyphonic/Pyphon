862d998f42d8853ffe222e18beb9fb2a6aeedcd2
Ford Fowler <fordjfowler@gmail.com>
2017-02-15 18:43:37 -0800

    working on message list css

--- pyphon/contacts/models.py
+++ pyphon/contacts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 # from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
-
+from datetime import datetime
 # Create your models here.
 
 
@@ -14,9 +14,22 @@ class Contact(models.Model):
         """Return title as string."""
         if self.name:
             return self.name
-        return str(self.number)
+        num = str(self.number)
+        return "({}) {}-{}".format(num[2:5], num[5:8], num[8:])
 
     def format_number(self):
         """Return formatted phone number."""
         num = str(self.number)
         return "({}) {}-{}".format(num[2:5], num[5:8], num[8:])
+
+    def most_recent_text(self):
+        body = self.texts.first().body
+        if len(body) > 24:
+            return self.texts.first().body[:20] + '...'
+        return body
+
+    def most_recent_time(self):
+        time = self.texts.first().time
+        if datetime.today().day == time.day:
+            return time.strftime('%I:%M %p')
+        return time.strftime('%m-%d')
